e4564450f6e8cf82a3d431fa92ca18e3e3919c71
LizethCarriQui <LizethCarriQui@LIIZETHQUICARRI>
2024-03-14 11:38:37 -0600

    Agregar archivos de proyecto.

---
+++ PhysicsCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace CalculatorPhysics
+{
+    class Program
+       {
+        static void Main(string[] args)
+           {
+       
+
+        Console.Write("Enter the height (in meters): ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+            double gravity = 9.8; //Acceleration due to gravity in m / s²
+            double speedFinal = Math.Sqrt(2 * gravity * height);
+      
+            Console.WriteLine($"Final velocity is: {speedFinal} m/s");
+                    Console.ReadKey();
+      }
+   }
+}
+
